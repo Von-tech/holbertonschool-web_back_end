4dadd1d23e7ea6f71d013730500667028c00d5ee
breezybrow <browder.bree@gmail.com>
2021-07-15 00:37:49 +0000

    Create a report Object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberofDepartments() {
-      return Object.keys(employeesList).length;
+      return Object.keys(this.allEmployees).length;
     },
   };
 }
