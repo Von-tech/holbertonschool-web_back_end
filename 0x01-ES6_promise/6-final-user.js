823389be5dfa21b83ecd7a42991a55825d8c38bf
breezybrow <browder.bree@gmail.com>
2021-07-16 16:37:41 +0000

    PLD help 7/16

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -10,11 +11,3 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     });
 
 }
-// return an array with the following structure:
-//[
-//    {
-//      status: status_of_the_promise,
-//      value: value or error returned by the Promise
-//    },
-//    ...
-//  ]
