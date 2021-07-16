96a2f110c1119d04c6842b81fccedd339b699fff
breezybrow <browder.bree@gmail.com>
2021-07-16 03:15:19 +0000

    Not complete

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+
+
+
+}
+// return an array with the following structure:
+//[
+//    {
+//      status: status_of_the_promise,
+//      value: value or error returned by the Promise
+//    },
+//    ...
+//  ]
