20dbbc789d51a9ac7afae11e4072e7093f04ef36
breezybrow <browder.bree@gmail.com>
2021-07-16 16:35:19 +0000

    PLD help 7/16

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-
-
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((repsonse) => {
+      response[1].value = `Error: ${response[1].reason.message}`;
+      delete response[1].reason;
+      return response;
+    });
 
 }
 // return an array with the following structure:
