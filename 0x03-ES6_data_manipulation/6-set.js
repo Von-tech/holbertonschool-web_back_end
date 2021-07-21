bb10546d81dbf0b8107df8300ca8528cc598d2dc
breezybrow <browder.bree@gmail.com>
2021-07-21 22:00:35 +0000

    Returning set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+export default function setFromArray(array) {
+  const setFromArray = (array) => new Set(array);
+}
+// return a set from an array
+// function must accept an array of any kind of element
