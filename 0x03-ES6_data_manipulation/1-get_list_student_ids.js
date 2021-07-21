ebc51382fc2ed9ca6c7cccfec9ebb36e93d39498
breezybrow <browder.bree@gmail.com>
2021-07-21 19:28:23 +0000

    Spacing error corrected

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(array) {
-  if (!Array.isArray(array)) return  [];
+  if (!Array.isArray(array)) return [];
   return array.map((i) => i.id);
 }
 // if arg is not an array, the func is returning an empty array
