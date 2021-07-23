a7f3ba58564be73424436aca79616fe05b2489d5
breezybrow <browder.bree@gmail.com>
2021-07-23 15:54:45 +0000

    Using checker to test forEach and for loop

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,23 @@
+export default function updateStudentGradeByCity(studentsList, city, newGrades) {
+  if (!(studentsList instanceof Array)) {
+    return [];
+}
+
+function addScore(student) {
+  for (const person in newGrades) {
+//  newGrades.forEach((person) => {
+    if (person.studentId === student.id) {
+      myArr.push(person.grade);
+    }
+  });
+  if (student.grade === undefined) {
+    student.grade = 'N/A';
+  }
+  return student;
+}
+const filteredStudentList = studentsList.filter((students) => students.location === city);
+return filteredStudentList.map(addScore);
+}
+// Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade
+// It should accept a list of students (from getListStudents), a city (String), and newGrades (Array of “grade” objects) as parameters
+// PLD HELP 7/23 10:50 A.M.
