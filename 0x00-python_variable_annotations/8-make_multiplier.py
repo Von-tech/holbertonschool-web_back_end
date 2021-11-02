7480f5147011c63fe8f3679a73dd93ffc83a0f93
breezybrow <browder.bree@gmail.com>
2021-11-02 22:44:36 +0000

    Task 8- Errors corrected for checker

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,9 +4,9 @@
 from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> Callable[[float, float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """  Returns a function that multiplies a float by multiplier """
     def multiply_func(new_mult: float) -> float:
         """ Return product """
         return multiplier * new_mult
-   return multiply_func
+    return multiply_func
