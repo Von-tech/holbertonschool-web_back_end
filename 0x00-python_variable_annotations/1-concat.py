f34a338dea920df134b9b582e1bf7b3d50d2039f
breezybrow <browder.bree@gmail.com>
2021-11-02 20:58:53 +0000

    Task 1 and 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env pytohn3
+""" Task 1: Basic annotations - concat """
+
+def concat(str1: str, str2: str) -> str:
+    """ Takes a str str1 and a str str2 as args & returns a concatenated str """
+    return str1 + str2
