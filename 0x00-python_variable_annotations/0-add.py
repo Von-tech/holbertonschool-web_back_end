0e86f1040b2743de24d90a5fbccdc71b43de153f
breezybrow <browder.bree@gmail.com>
2021-11-02 21:09:41 +0000

    Resub Task 1 and 2 pep8 comp

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pytohn3
 """ Task 0: Basic annotations - add """
 
+
 def add(a: float, b: float) -> float:
     """ Takes float a & float b as args & returns their sum as a float """
     return a + b
