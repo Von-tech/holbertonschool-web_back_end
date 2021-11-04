133b812c62f73ed91df5a36f4c8f3d0757845a3a
breezybrow <browder.bree@gmail.com>
2021-11-04 01:25:59 +0000

    Last task, typo error fixed

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Task 4: Tasks; take code from wait_n and alter it into task_wait_n """
 import asyncio
-import typing import List
+from typing import List, Any
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
@@ -13,7 +13,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     for i in range(n):
         allDelays.append(task_wait_random(max_delay))
 
-    List_of_tasks: List[float] = []
+    List_of_tasks: List[Any] = []
 
     for results in asyncio.as_completed(allDelays):
         """ wait for as_completed to return """
