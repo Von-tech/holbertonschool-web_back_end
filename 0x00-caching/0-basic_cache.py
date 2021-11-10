08667c91c5ed3976147770fcfc65c84b6286768e
breezybrow <browder.bree@gmail.com>
2021-11-10 18:40:44 +0000

    Task 0: basic dict

--- 0x00-caching/0-basic_cache.py
+++ 0x00-caching/0-basic_cache.py
@@ -4,12 +4,12 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ Basic cache system "that inherits from the parent class """
+    """ Basic cache system that inherits from the parent class """
     def put(self, key, item):
-        """ Puts key and item in cache dictionary """
+        """ Puts key/item in cache dict """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Gets key from cache dictionary if it exists """
+        """ Gets key from cache dict if it exists """
         return self.cache_data.get(key)
