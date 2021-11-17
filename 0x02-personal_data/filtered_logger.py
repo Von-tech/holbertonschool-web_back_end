30c86598a3e4a71b41d28cc9125eefb83b6931b5
breezybrow <browder.bree@gmail.com>
2021-11-17 19:49:55 +0000

    Task 1: filter val in incoming log records

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 """ Task 0: Write a function that returns the log message obfuscated """
+""" Task 1: Update class to accept a list of strs fields constructor arg """
+"""Implement the format method to filter values in incoming log records """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Creating an instance of class above """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the record - self.FORMAT """
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record),
+            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str],
