b946cf7195e6703aec3d1a245102eb28bacf8e80
breezybrow <browder.bree@gmail.com>
2021-11-22 04:09:46 +0000

    [edit] Task 3: format back to original vagrant copy

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -24,9 +24,7 @@ class Auth:
 
 def authorization_header(self, request=None) -> str:
     """ Public method that return None - request """
-    if request is None
-    return None
-    if request.headers.get('Authorization') is None:
+    if request is None or request.headers.get('Authorization') is None:
         return None
     return request.headers.get('Authorization')
 
