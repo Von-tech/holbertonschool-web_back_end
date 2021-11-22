7d81db5eee4a2ec51a4c3ebf213e506a25ba8e85
breezybrow <browder.bree@gmail.com>
2021-11-22 04:03:19 +0000

    [edit] Task 3: format back to original vagrant copy

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -11,22 +11,19 @@ class Auth:
 
         backslash = '/'
 
-        if excluded_paths is None:
+        if path is None or excluded_paths is None:
             return True
 
-        if excluded_paths is not len(excluded_paths):
+        if not excluded_paths:
             return True
 
-        if path is None:
-            return True
+        if path[-1] != backslash:
+            path += backslash
 
-        if path in excluded_paths:
+        if path not in excluded_paths:
+            return True
+        else:
             return False
-
-        if path + backslash in excluded_paths:
-            return False
-
-        return True
 
 
 def authorization_header(self, request=None) -> str:
