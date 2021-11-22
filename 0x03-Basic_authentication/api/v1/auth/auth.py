c1ee22136b261f2e810df827de6e63205d5b68e6
breezybrow <browder.bree@gmail.com>
2021-11-22 03:57:50 +0000

    Task 3: add new dir, auth class, and __init__ file

---
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Task 3: Auth class """
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ This class is used to manage the API authentication """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Check for excluded paths """
+
+        backslash = '/'
+
+        if excluded_paths is None
+        return True
+
+        if excluded_paths is not len(excluded_paths)
+        return True
+
+        if path is None:
+            return True
+
+        if path in excluded_paths
+        return False
+
+        if path + backslash in excluded_paths:
+            return False
+
+        return True
+
+
+def authorization_header(self, request=None) -> str:
+    """ Public method that return None - request """
+    if request is None
+    return None
+    if request.headers.get('Authorization') is None:
+        return None
+    return request.headers.get('Authorization')
+
+
+def current_user(self, request=None) -> TypeVar('User'):
+    """ Public method that return None - request """
+    return None
