37c0884995c2d04b70d7049a586e8deeacc3f9ef
breezybrow <browder.bree@gmail.com>
2021-11-22 18:03:30 +0000

    Task 7: Base64 part

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,15 @@ from models.user import User
 
 class BasicAuth(Auth):
     """ New class that inherits from Auth """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header:
+                                            str) -> str:
+        """ Method that returns the Base64 part of the Authorization header """
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is False:
+            return None
+        if authorization_header.startswith('Basic ') is False:
+            return None
+        else:
+            return authorization_header[6:]
