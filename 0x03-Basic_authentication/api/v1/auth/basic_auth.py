0ea4295686bf12415d3d329c0d6d6a62ad309dbe
breezybrow <browder.bree@gmail.com>
2021-11-22 17:35:19 +0000

    Add basic_auth.py

---
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Task 6: Basic Auth """
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class BaicAuth(Auth):
+    """ New class that inherits from Auth """
