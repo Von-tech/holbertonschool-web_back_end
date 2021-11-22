cc65faac3e906e9e50a28c2977b2a17c545c67b3
breezybrow <browder.bree@gmail.com>
2021-11-22 17:27:30 +0000

    Task 5-6: validating all requests to secure API

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -12,6 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = os.getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BaiscAuth
+    auth = BaicAuth()
+excluded_paths = ['/api/v1/status/',
+                  '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request() -> Dict:
+    """ Filtering each request with handler """
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        elif auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(401)
