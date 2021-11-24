5d56eb30dab63431baeb7973895a93c34b0e2837
breezybrow <browder.bree@gmail.com>
2021-11-24 14:47:56 +0000

    Server already in use; not curling properly

--- 0x04-Session_authentication/api/v1/app.py
+++ 0x04-Session_authentication/api/v1/app.py
@@ -25,7 +25,8 @@ elif auth_type == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 excluded_paths = ['/api/v1/status/',
-                  '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                  '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                  '/api/v1/auth_session/login/']
 
 
 @app.before_request
@@ -34,6 +35,8 @@ def before_request() -> Dict:
     if auth and auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
+        if auth.session_cookie(request) is None:
+            sbort(401)
         elif auth.current_user(request) is None:
             abort(403)
         request.current_user = auth.current_user(request)
