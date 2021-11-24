ec112b5213ed28ed130cb6bde7643a0db212b87e
breezybrow <browder.bree@gmail.com>
2021-11-24 13:39:57 +0000

    Task 0: update app.py and users.py

--- 0x04-Session_authentication/api/v1/app.py
+++ 0x04-Session_authentication/api/v1/app.py
@@ -33,6 +33,7 @@ def before_request() -> Dict:
             abort(401)
         elif auth.current_user(request) is None:
             abort(403)
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(401)
--- 0x04-Session_authentication/api/v1/views/users.py
+++ 0x04-Session_authentication/api/v1/views/users.py
@@ -25,9 +25,11 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None:
+    if user_id is None or (user_id == "me" and request.current_user is None):
         abort(404)
     user = User.get(user_id)
+    if user_id == "me" and request.current_user is not None:
+        user = User.get(request.current_user.__dict__.get("id"))
     if user is None:
         abort(404)
     return jsonify(user.to_json())
