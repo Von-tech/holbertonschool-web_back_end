c9884498f1746f62393d220c9508179c43e4299f
breezybrow <browder.bree@gmail.com>
2021-11-24 13:57:12 +0000

    Adding imports

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Task 1: Empty session """
-api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
