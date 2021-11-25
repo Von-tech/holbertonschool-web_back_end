437965ed41e98b66367c0d34dcb25f07437487ab
breezybrow <browder.bree@gmail.com>
2021-11-25 04:12:42 +0000

    Update SessionAuth class

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -10,7 +10,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ creates a Session ID for a user_id """
+        """ Creates a Session ID for a user_id """
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
@@ -18,7 +18,25 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ returns a user ID based on a Session ID """
+        """ Returns a user ID based on a Session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        elif self.user_id_for_session_id(session_id) is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
