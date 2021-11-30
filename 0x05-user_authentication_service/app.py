f6adfcee49aea00d2b6e0b8530c6cdf45aca4ac0
breezybrow <browder.bree@gmail.com>
2021-11-30 23:06:55 +0000

    Task 7

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -14,5 +14,17 @@ def begin():
     return jsonify({'message': 'Bienvenue'})
 
 
+@app.route("/users", methods=["POST"])
+def users():
+    """ Method implements the end-point to register a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        if AUTH.register_user(email, password):
+            return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
