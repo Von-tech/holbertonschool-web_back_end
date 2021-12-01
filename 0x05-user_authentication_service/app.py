0c3e353c6093f42fd4debfc691a4ca5f19bb1011
breezybrow <browder.bree@gmail.com>
2021-12-01 23:40:32 +0000

    Cleaning up code, corrections

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -27,26 +27,43 @@ def users():
 
 
 @app.route("/sessions", methods=["POST"])
-def login():
+def login() -> str:
     """ Method to log in user """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if not email or not password or not AUTH.valid_login(email, password):
+    email = request.form.get('email')
+    pw = request.form.get('password')
+
+    if not email or not pw or not AUTH.valid_login(email, password):
         abort(401)
-    session_id = AUTH.create_session(email=email)
-    response = jsonify({"email": email, "message": "logged in"})
+
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
-    return response
+    return resp
 
 
-@qpp.route("/sessions", methods=["DELETE"])
+@app.route("/sessions", methods=["DELETE"])
 def logout():
     """ Method to log out """
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect("/", 302)
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+    abort(403)
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """ User profile method """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        try:
+            user = AUTH.get_user_from_session_id(session_id)
+            resp = jsonify({"email": "{}".format(user.email)})
+            return resp, 200
+        except ValueError:
+            return None
     abort(403)
 
 
