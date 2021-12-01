f922a7dd2f7b08c407d41e7890693ecd816c6c6f
breezybrow <browder.bree@gmail.com>
2021-12-01 22:54:03 +0000

    [edit] Final changes to auth.py

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
-            self._db.update_user(found_user.id, reset_token=reset_token)
+            self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
             raise ValueError
