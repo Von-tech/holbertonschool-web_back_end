6f305435b30903d2d4cf764ca95b7bb7881f5b80
breezybrow <browder.bree@gmail.com>
2021-12-06 23:24:06 +0000

    Task 0: pushing to test in sandbox env, pycodestyle is invalid

---
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Task 0. Parameterize a unit test """
+import unittest
+from unittest import mock
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class that inherits from unittest.TestCase """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Method to test that the method returns what it is supposed to """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+       ({}, ("a")),
+       ({"a": 1}, ("a", "b")),
+   ])
