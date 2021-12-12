c8b1f6f8f3f654cb4eb1d46ea64937587eda8e6e
breezybrow <browder.bree@gmail.com>
2021-12-12 23:57:34 +0000

    Last commit for the day

---
+++ 0x01-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Task 2: Get locale from request """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+from typing import Text
+
+app = Flask(__name__)
+babel = Babel()
+gettext.__doc__ = "Documentation for gettext"
+
+
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """ Returning our html page """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Getting locale from request.accept_languages """
+    locale = request.args.get("locale")
+    if locale is not None and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
