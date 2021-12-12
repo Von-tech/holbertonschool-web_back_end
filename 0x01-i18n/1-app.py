f67a713e90a1325546c29f8a7a2b41144e519bb2
breezybrow <browder.bree@gmail.com>
2021-12-12 21:55:53 +0000

    Task 1-2, setup/get locale: not sure if index.html should be getting updated

---
+++ 0x01-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Task 1: Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel()
+
+
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Returning our html page """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x01-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Task 2: Get locale from request """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel()
+
+
+class Config(object):
+    """ Class will configure available languages in the app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ Returning our html page """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ Getting locale from request.accept_languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
