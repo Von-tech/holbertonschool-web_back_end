4bfafaf2b6d63675e07d2c00cc9401852df3640e
breezybrow <browder.bree@gmail.com>
2021-12-12 21:38:51 +0000

    Task 0: Basic setup

---
+++ 0x01-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Task 0: Setup a basic Flask app """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Returning our html page """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
