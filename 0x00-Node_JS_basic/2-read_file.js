7c9b1cff8982dfc181fdb6d8555142b16d4efae1
breezybrow <browder.bree@gmail.com>
2022-01-05 21:15:33 +0000

    Got yagit add 2-read_file.js !

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const countStudents = (path) => {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
-  }    
+  }
   const FileContents = fs.readFileSync(path, 'utf8');
   let arr = FileContents.toString().split(/\r?\n/);
   arr = arr.filter((line) => line !== '');
