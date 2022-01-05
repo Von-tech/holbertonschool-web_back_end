6b71667f7775bc2bdbf105a2ad0c1a96e248b9b9
breezybrow <browder.bree@gmail.com>
2022-01-05 18:04:07 +0000

    Linter error resolved

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -3,12 +3,12 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', () => {
-    const inputData = process.stdin.read();
-    if (inputData !== null) {
-      process.stdout.write(`Your name is: ${inputData}`);
-    }
+  const inputData = process.stdin.read();
+  if (inputData !== null) {
+    process.stdout.write(`Your name is: ${inputData}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
