92726060dd5221e15df3d99e6a8ed17dd3775dcd
breezybrow <browder.bree@gmail.com>
2022-01-05 20:58:54 +0000

    Comments moved to accomadate linter

--- 0x00-Node_JS_basic/2-read_file.js
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
 
-// separated into sections from task reqs
 const countStudents = (path) => {
   if (!fs.existsSync(path)) {
     throw Error('Cannot load the database');
   }    
   const FileContents = fs.readFileSync(path, 'utf8');
   let arr = FileContents.toString().split(/\r?\n/);
-  // split the array into indiv lines 
   arr = arr.filter((line) => line !== '');
   arr.shift();
-  // remove first element in the array and return the elem
   console.log(`Number of students: ${arr.length}`);
-  // freecodecamp.org/news/javascript-split-how-to-split-a-string-into-an-array-in-js/
-  // youtube.com/watch?v=4_iT6EGkQfk
-  // blog.kevinchisholm.com/javascript/array-prototype/shift/    
   const locateCS = arr.filter((line) => line.endsWith('CS')).map((line) => {
     const CSpeeps = line.split(',');
     return CSpeeps[0];
@@ -27,3 +21,11 @@ const countStudents = (path) => {
   console.log(`Number of students in SWE: ${locateSWE.length}. List: ${locateSWE.join(', ')}`);
 };
 module.exports = countStudents;
+// line 3: note - separated into sections from task reqs
+// line 8: split the array into indiv lines
+// line 10: remove first element in the array and return the elem
+
+// Resources
+// freecodecamp.org/news/javascript-split-how-to-split-a-string-into-an-array-in-js/
+// youtube.com/watch?v=4_iT6EGkQfk
+// blog.kevinchisholm.com/javascript/array-prototype/shift/
