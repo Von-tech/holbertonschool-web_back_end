1a669c7004eb3989f3f6ed78169438ae9e47b29b
breezybrow <browder.bree@gmail.com>
2022-01-13 03:28:09 +0000

    Add 7-8

---
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+const chai = require("chai");
+const request = require('request')
+
+describe('Suite for the index page', () => {
+    describe('GET /', () => {
+	it('endpoint: checking output of GET /', (done) => {
+	    request('http://localhost:7865/', (err, response, body) => {
+		expect(response.statusCode).to.equal(200)
+		expect(body).to.equal('Welcome to the payment system')
+		done()
+	    });
+	});
+    });
+})
