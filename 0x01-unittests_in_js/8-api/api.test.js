9f8b17c91a47ca430e74f2ca45ecf955f2197835
breezybrow <browder.bree@gmail.com>
2022-01-13 03:43:06 +0000

    [edit] api.test.js

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -7,7 +7,7 @@ describe('Suite for the index page', () => {
 	    request('http://localhost:7865/', (err, response, body) => {
 		expect(response.statusCode).to.equal(200)
 		expect(body).to.equal('Welcome to the payment system')
-		done()
+		done();
 	    });
 	});
     });
